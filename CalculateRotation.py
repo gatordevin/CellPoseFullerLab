b687abe9870f07857394661d2499564c595b8187
gatordevin <gatordevin@gmail.com>
2022-12-09 21:58:34 -0500

    Made some code changes

---
+++ CalculateRotation.py
@@ -0,0 +1,5 @@
+# Calcualte the rotation of a polygon represented in the .roi file format
+folder = "C:/Users/gator/OneDrive - University of Florida/10x images for quantification/PM NEUN FOR QUANTIFICATION"
+
+def calculate_rotation(polygon):
+    
